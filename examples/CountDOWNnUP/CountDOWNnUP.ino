d6fab06106cac21ffa563bc9a7e292a0df14fa97
Aniruddh Dalvi <aniruddh.dalvi@gmail.com>
2022-07-25 16:19:06 +0530

    modified:   examples/CountDOWNnUP/CountDOWNnUP.ino

--- examples/CountDOWNnUP/CountDOWNnUP.ino
+++ examples/CountDOWNnUP/CountDOWNnUP.ino
@@ -1,7 +1,7 @@
 #include <segDec.h>
 
 #define MODE 1 // 1 -> common Cathode/GND; 0 -> common Anode/Vcc
-#define DELAY 1000 // by default the delay is set to 1sec, the functions can overloade to a custom delay.
+#define DELAY 1000 // by default the delay is set to 1sec, the functions can overloaded to a custom delay.
 
 
 int a = 30;
